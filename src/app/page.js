d5738ed59fe59afa6c3bac6d46ca9206be4af225
TusharJade <79801546+TusharJade@users.noreply.github.com>
2024-03-08 23:46:11 +0530

    feat: added landing page

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,18 @@
+import Navbar from "@/components/Navbar";
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <main>
-      Main Page
-    </main>
+    <div className="h-screen bg-[#f9f9f9]">
+      <Navbar />
+      <section className="text-center">
+        <div className="font-semibold text-[2.625rem] mt-7">Photo editor</div>
+        <p className="text-[1.375rem] text-[#47474f] mb-8">
+          Add text, stickers, effects and filters to your photos. Edit your
+          photos online
+        </p>
+        <Button variant="default">Select Image</Button>
+      </section>
+    </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,33 @@
+import Image from "next/image";
+import SpeedyBrand from "../../public/assets/SpeedyBrand.svg";
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <nav className="border-b-[1.5px] h-[4rem] flex justify-between bg-white">
+      <div className="flex h-full items-center gap-6 ml-6">
+        <Image src={SpeedyBrand} className="cursor-pointer" alt="logo" />
+        <div className="font-medium cursor-pointer">Image Editor</div>
+      </div>
+      <div className="flex items-center justify-center mr-5 gap-4">
+        <Link className="text-[14.5px] font-semibold" href="/login">
+          Login
+        </Link>
+        <Link
+          className="font-medium text-white bg-[#fe5829] py-[0.3125rem] px-3 rounded-md text-[14.5px]"
+          href="/singup"
+        >
+          Sign up
+        </Link>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
+
+{
+  /* <div className="bg-[#fe5829] w-12 h-12 rounded-full flex items-center justify-center text-[1.9rem] pt-[2px] text-white cursor-pointer">
+          T
+        </div> */
+}
